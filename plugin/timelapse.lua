169aff348b10adfbf673246609c84c6a484b8a29
fkys <sodiumthiosulfat@gmail.com>
2024-04-23 21:24:10 +0700

    Added keypress option
    Each time you press any key it print line by line, still not what i wanted originally but whatever

--- plugin/timelapse.lua
+++ plugin/timelapse.lua
@@ -3,10 +3,10 @@ if vim.fn.has("nvim-0.7.0") == 0 then
   return
 end
 
-local function current_row()
-  local window = vim.api.nvim_get_current_win()
-  local cursor = vim.api.nvim_win_get_cursor(window)
-  return cursor[1]
+local animation_paused = false
+
+local function toggle_animation_pause()
+  animation_paused = not animation_paused
 end
 
 local function buf0_str()
@@ -14,30 +14,48 @@ local function buf0_str()
   return table.concat(content, "\n")
 end
 
-local function write_to_buf_line(text, buf, filetype, delay)
+local function write_to_buf_line(text, buf, filetype, delay, keypress)
   vim.api.nvim_command("tabnew")
   vim.api.nvim_command("buffer " .. buf)
   vim.api.nvim_buf_set_option(buf, "filetype", filetype)
 
   delay = delay or 100
+  delay = delay or false
   local lines = vim.split(text, "\n")
+
   for _, line in ipairs(lines) do
     vim.api.nvim_buf_set_lines(buf, -1, -1, false, { line })
     vim.api.nvim_command("redraw")
     vim.api.nvim_command("sleep " .. delay .. "m")
     vim.cmd("norm! j")
+
+    if keypress then
+    vim.cmd("norm! jjjj")
+      while animation_paused do
+        vim.wait(50)
+      end
+       local c = vim.fn.getchar()
+       if c == 0 then
+           toggle_animation_pause()
+       end
+    end
+
   end
 end
 
+
 local function timelapse(opts)
   local delay = nil
-  if opts.args ~= "" then
-    delay = tonumber(opts.args)
-  end
   local buf = vim.api.nvim_create_buf(false, true)
   local filetype = vim.bo.filetype
   local text = buf0_str()
-  write_to_buf_line(text, buf, filetype, delay)
+  if opts.args ~= "" then
+    delay = tonumber(opts.args)
+    if delay == nil then
+      write_to_buf_line(text, buf, filetype, nil, true)
+    end
+  end
+  write_to_buf_line(text, buf, filetype, delay, nil)
 end
 
 vim.api.nvim_create_user_command(
@@ -45,3 +63,4 @@ vim.api.nvim_create_user_command(
   timelapse,
   { nargs = "?", desc = "Make a timelapse-like effects" }
 )
+
