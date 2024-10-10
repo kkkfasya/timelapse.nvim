6745e91054a9412ecf064c7b26a58363e94767cb
Kamal Fasya <kamalfasya01@gmail.com>
2024-10-10 13:20:40 +0700

    LESGOOOO

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # timelapse.nvim
-Timelapse-like effect in neovim
+Timelapse-like effect in neovim  
 if you ever watch [one](https://www.youtube.com/watch?v=vkUwT9U1GzA) of bisqwit's videos you know what i mean  
-The effect that i want is not quite achieved yet
 
-## Instalation
+## Installation
 Use your favorite plugin manager, for example:
 ### [vim-plug](https://github.com/junegunn/vim-plug)
 ```vim
 call plug#begin()
 
-Plug 'fkys/timelapse.nvim'
+Plug 'kkkfasya/timelapse.nvim'
 
 call plug#end()
 ```
 ## Usage
-```Haskell
-:Timelapse <delay_ms || boolean>
-e.g :Timelapse 300 -- will print automatically with 300ms delay
-e.g :Timelapse true -- will print each time any key is pressed
+```Lua
+:Timelapse -- will use default delay (30ms)
+
+:Timelapse <delay_ms>
+e.g :Timelapse 10 -- 10ms delay for each char
 ```
+Press ```q``` or ```<ESC>``` to exit  
+To exit while animation still running, ```Ctrl + C``` then press any key above
 
 > shoutout to ChatGPT yo, couldn't made it without you cuz i was too lazy to read the docs
--- plugin/timelapse.lua
+++ plugin/timelapse.lua
@@ -1,66 +1,61 @@
+-- TODO: add pause feature
 if vim.fn.has("nvim-0.7.0") == 0 then
-  vim.api.nvim_err_writeln("timelapse.nvim requires at least nvim-0.7.0.1")
-  return
+	vim.api.nvim_err_writeln("timelapse.nvim requires at least nvim-0.7.0.1")
+	return
 end
 
-local animation_paused = false
-
-local function toggle_animation_pause()
-  animation_paused = not animation_paused
+local function get_current_buffer()
+	local content = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	return table.concat(content, "\n")
 end
 
-local function buf0_str()
-  local content = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  return table.concat(content, "\n")
+local function write_animation(text, buf, filetype, delay)
+	-- Make new scratch buffer to scratch them texts
+	vim.api.nvim_command("buffer " .. buf)
+	vim.api.nvim_buf_set_option(buf, "filetype", filetype)
+	vim.api.nvim_buf_set_option(buf, "buftype", "nofile")
+	vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+	vim.api.nvim_buf_set_option(buf, "swapfile", false)
+
+	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":q!<CR>", { noremap = true, silent = true })
+	vim.api.nvim_buf_set_keymap(buf, "n", "<Esc>", ":q!<CR>", { noremap = true, silent = true })
+
+	delay = delay or 30
+
+	local cursor_row = 1
+	local cursor_col = 1
+	local current_line = ""
+
+	for i = 1, #text do
+		local char = text:sub(i, i)
+		if char == "\n" then
+			current_line = ""
+			cursor_row = cursor_row + 1
+			vim.api.nvim_buf_set_lines(buf, -1, -1, false, { current_line })
+		else
+			current_line = current_line .. char
+			cursor_col = cursor_col + 1
+			vim.api.nvim_buf_set_lines(buf, -2, -1, false, { current_line })
+		end
+		vim.api.nvim_win_set_cursor(0, { cursor_row, cursor_col - 1 }) -- Yeah fuck lua 1-based index
+		vim.api.nvim_command("redraw")
+		vim.api.nvim_command("sleep " .. delay .. "m")
+	end
+	vim.api.nvim_buf_set_option(buf, "modifiable", false) -- modifiable enable sufferable
 end
-
-local function write_to_buf_line(text, buf, filetype, delay, keypress)
-  vim.api.nvim_command("tabnew")
-  vim.api.nvim_command("buffer " .. buf)
-  vim.api.nvim_buf_set_option(buf, "filetype", filetype)
-
-  delay = delay or 100
-  delay = delay or false
-  local lines = vim.split(text, "\n")
-
-  for _, line in ipairs(lines) do
-    vim.api.nvim_buf_set_lines(buf, -1, -1, false, { line })
-    vim.api.nvim_command("redraw")
-    vim.api.nvim_command("sleep " .. delay .. "m")
-    vim.cmd("norm! j")
-
-    if keypress then
-    vim.cmd("norm! jjjj")
-      while animation_paused do
-        vim.wait(50)
-      end
-       local c = vim.fn.getchar()
-       if c == 0 then
-           toggle_animation_pause()
-       end
-    end
-
-  end
-end
-
 
 local function timelapse(opts)
-  local delay = nil
-  local buf = vim.api.nvim_create_buf(false, true)
-  local filetype = vim.bo.filetype
-  local text = buf0_str()
-  if opts.args ~= "" then
-    delay = tonumber(opts.args)
-    if delay == nil then
-      write_to_buf_line(text, buf, filetype, nil, true)
-    end
-  end
-  write_to_buf_line(text, buf, filetype, delay, nil)
+	local delay = nil
+	local buf = vim.api.nvim_create_buf(false, true)
+	local filetype = vim.bo.filetype
+	local text = get_current_buffer()
+	if opts.args ~= "" then
+		delay = tonumber(opts.args)
+		if delay == nil then
+			write_animation(text, buf, filetype, nil)
+		end
+	end
+	write_animation(text, buf, filetype, delay)
 end
 
-vim.api.nvim_create_user_command(
-  "Timelapse",
-  timelapse,
-  { nargs = "?", desc = "Make a timelapse-like effects" }
-)
-
+vim.api.nvim_create_user_command("Timelapse", timelapse, { nargs = "?", desc = "Make a timelapse-like effects" })
